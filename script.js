d866a7473e12f7429c70d13f82511967519a4ccb
Bryan Jones <thebeard@me.com>
2018-02-23 19:14:56 -0800

    Mobile not working on real device/only browser-sizing

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
  
-/*======= Show /Hide Nav Bar ========*/
+/*======= Show / Hide Navbar ========*/
     $('nav ul li').hover(function(){
         $(this).find('ul>li').stop().slideToggle(400);
     });
 
 
+/*======= Scroll to About Us ========*/
+    $("a[href='#bottom']").click(function() {
+        $("html, body").animate({scrollTop: $(".aboutUs").offset().top}, 'slow');
+      });
 
 
-    
+
+    //   $('nav ul li').click(function(){
+    //     $(this).find('ul>li').stop().slideToggle(400);
+    // });
 });
     
