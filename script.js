87521fa3a1186527a9f14fd936355362fd8a0dce
Bryan Jones <thebeard@me.com>
2018-02-23 17:20:44 -0800

    Refactoring navigation

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
  
 /*======= Show /Hide Nav Bar ========*/
-    $('#navBar ul li').hover(function(){
+    $('nav ul li').hover(function(){
         $(this).find('ul>li').stop().slideToggle(400);
     });
 
 
 
 
+    
 });
     
